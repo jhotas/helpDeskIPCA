fc14d8c55bb8089341c59a7d4289b54945b5796e
Jean Oliveira <jcwilbertt@gmail.com>
2023-10-23 12:49:49 +0100

    first commit

--- main.cs
+++ main.cs
@@ -6,8 +6,51 @@ using System.Threading.Tasks;
 
 namespace helpDeskIPCA
 {
-    internal class main
+    class Assistencia
     {
-        Console.WriteLine("Teste");
+        public int ID;
+        public string tipo;
+        public string estado;
+        public string descricao;
+        public string operador;
+        public string cliente;
+        public int avaliacao;
+
+        public Assistencia(int ID, string tipo, string estado, string descricao, string operador, string cliente)
+        {
+            this.ID = ID;
+            this.tipo = tipo;
+            this.estado = estado;
+            this.descricao = descricao;
+            this.operador = operador;
+            this.cliente = cliente;
+        }
+
+        public void avaliar(string novoEstado, int avaliacaoAtendimento)
+        {
+            estado = novoEstado;
+            avaliacao = avaliacaoAtendimento;
+            Console.WriteLine($"A assistência com ID {ID} foi alterada para {estado}.\n");
+            Console.WriteLine($"Você avaliou em {avaliacao} o atendimento de {operador}.\n");
+        }
+    }
+
+    class main
+    {
+        static void Main(string[] args)
+        {
+            int escolha;
+            Console.WriteLine(" ||| Helpdesk IPCA ||| ");
+            Console.WriteLine("Selecione uma das opções: \n1. Obter assistência\n2. Verificar estado de uma assistência\n" +
+                "3. Resolução de problemas\n4. Sair");
+            escolha = int.Parse(Console.ReadLine());
+            while (escolha != 4)
+            {
+                if (escolha == 1)
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
     }
 }
